7e2eb688cec2b2d4d7ebcaf22d2366d772cae58e
Arapaimace <adarshv06.us@gmail.com>
2025-12-03 14:25:17 -0800

    created clinics table

---
+++ server/db/schema/clinics.sql
@@ -0,0 +1,13 @@
+CREATE TYPE experience_level_enum AS ENUM('beginner', 'intermediate', 'experience');
+
+CREATE TABLE clinics (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    location TEXT,
+    time TIMESTAMP,
+    date DATE,
+    attendees INTEGER,
+    experience_level experience_level_enum,
+    parking TEXT;
+)
