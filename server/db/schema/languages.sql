f3d053ee51bdadf64bb104c57c8b003dac6b6373
Yousef <theyousefkhan@gmail.com>
2025-12-04 16:58:36 -0800

    Added workshop langs and volunteer lang

--- server/db/schema/languages.sql
+++ server/db/schema/languages.sql
@@ -22,3 +22,21 @@ CREATE TABLE IF NOT EXISTS public.clinic_languages
     FOREIGN KEY (clinic_id) REFERENCES public.clinics(id),
     FOREIGN KEY (language_id) REFERENCES public.languages(id)
 );
+
+CREATE TABLE IF NOT EXISTS public.workshop_languages
+(
+    workshop_id INT NOT NULL,
+    language_id INT NOT NULL,
+    PRIMARY KEY (workshop_id, language_id),
+    FOREIGN KEY (workshop_id) REFERENCES public.workshops(id),
+    FOREIGN KEY (language_id) REFERENCES public.languages(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.volunteer_language
+(
+    volunteer_id INT NOT NULL,
+    language_id INT NOT NULL,
+    PRIMARY KEY (volunteer_id, language_id),
+    FOREIGN KEY (volunteer_id) REFERENCES public.volunteers(id),
+    FOREIGN KEY (language_id) REFERENCES public.languages(id)
+);
