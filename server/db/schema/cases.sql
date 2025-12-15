b98825b5807132a40b26e78c072e356625ea5493
Joshua Sullivan <joshee.sullivan@gmail.com>
2025-12-14 23:12:12 -0800

    fix: tweaked constraint

--- server/db/schema/cases.sql
+++ server/db/schema/cases.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS cases CASCADE;
 CREATE TABLE cases (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT,
   email_contact TEXT NOT NULL
 );
