9101635aba83550ef9c55832f35b8b091804962e
Arapaimace <adarshv06.us@gmail.com>
2026-01-12 22:58:30 -0800

    more skeleton

--- client/src/components/eventManagement/EventManagement.jsx
+++ client/src/components/eventManagement/EventManagement.jsx
@@ -1,15 +1,49 @@
 import {
     Card,
     VStack,
-   // HStack,
-   // Button,
-    //Box
+    Tabs,
+    TabPanels,
+    TabList,
+    Tab,
+    HStack,
+    Button,
+    Box,
+    Input,
+    TabPanel
 } from "@chakra-ui/react";
 
+//import { createEvent };
 
 export const EventManagement = () => {
     return (
         <VStack>
+            <HStack>
+                <Text> ELDR </Text>
+                <Input placeholder = "search sign placeholder"></Input>
+                <Text> Profile symbol placeholder </Text>
+
+            </HStack>
+            <HStack>
+                <Tabs>
+                    <TabList>
+                        <Tab>Clinics & Workshops</Tab>
+                        <Tab>Cases</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            {/*
+                            Implement Clinics & Workshops here
+                            */}
+                        </TabPanel>
+                        <TabPanel>
+                            {/* 
+                            Figure out what to do with Cases here
+                            */}
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+
+            </HStack>
             <VStack>
                 {/*
                 placeholders for eventual mapping of events onto individual cards
---
+++ client/src/components/eventManagement/createEvent.jsx
@@ -0,0 +1,22 @@
+import {
+    VStack,
+    Box,
+    Text,
+    Card,
+    Button
+} from "@chakra-ui/react";
+
+
+
+export const createEvent = () => {
+    return (
+        <VStack>
+            {/*
+            skeleton for future implementation
+            */}
+            <Card>
+                HELLO!
+            </Card>
+        </VStack>
+    );
+};
