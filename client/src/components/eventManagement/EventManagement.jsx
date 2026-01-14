7530e1c451930b6cbc5f96dea0158c217c96f6a3
Arapaimace <adarshv06.us@gmail.com>
2026-01-14 09:59:11 -0800

    added example clinic

--- client/src/components/eventManagement/EventManagement.jsx
+++ client/src/components/eventManagement/EventManagement.jsx
@@ -1,6 +1,7 @@
 import {
     Card,
     VStack,
+    HStack,
     Tabs,
     TabPanels,
     TabList,
@@ -15,13 +16,18 @@ import {
     InputGroup, 
     InputLeftElement,
     Icon,
-    useDisclosure
+    useDisclosure,
+    Tag
 } from "@chakra-ui/react";
 
 
 import { CiSearch } from "react-icons/ci";
 import { HiMiniPlusCircle } from "react-icons/hi2";
 import { CgProfile } from "react-icons/cg";
+import { IoCalendarSharp } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoPersonSharp } from "react-icons/io5";
+
 import { CreateEvent } from "./CreateEvent.jsx";
 
 export const EventManagement = () => {
@@ -30,7 +36,7 @@ export const EventManagement = () => {
     return (
         <VStack w="100%" h="100%" minH={"100vh"} bg="#E8E8E8">
             <Flex w="100%" align="center" px="2%" py="2%">
-                <Text fontWeight="bold" fontSize="xl">ELDR</Text>
+                <Text fontWeight="bold" fontSize="xl"> ELDR </Text>
                 <InputGroup 
                     maxW="70%" 
                     mx="auto"
@@ -48,9 +54,9 @@ export const EventManagement = () => {
             </Flex>
             <Tabs w = "100%">
                 <Flex justifyContent = "center">
-                    <TabList gap = {4}>
-                        <Tab>Clinics & Workshops</Tab>
-                        <Tab>Cases</Tab>
+                    <TabList gap = {40}>
+                        <Tab fontSize = "lg" fontWeight="bold"> Clinics & Workshops </Tab>
+                        <Tab fonSize = "lg" fontWeight = "bold"> Cases </Tab>
                     </TabList>
                 </Flex>
                 <TabPanels>
@@ -61,6 +67,7 @@ export const EventManagement = () => {
                             align = "center" 
                             p = "2%" 
                             gap = "2%"
+                            mt = "2%"
                         >
                             <Select
                                 placeholder="Sort By"
@@ -100,17 +107,84 @@ export const EventManagement = () => {
                             </Button>
                             <CreateEvent isOpen={isOpen} onClose={onClose} />
                         </Flex>
-                        <VStack p = {4}>
-                            <Card>
-                                example
-                            </Card>
-                            <Card>
-                                example2
-                            </Card>
-                            <Card>
-                                example3
-                            </Card>
-                        </VStack>
+                        <Tabs>
+                            <Flex justifyContent="center" mt = "1%">
+                                <TabList gap = {10}>
+                                    <Tab> Upcoming Events </Tab>
+                                    <Tab> Event Archive </Tab>
+                                </TabList>
+                            </Flex>
+                            <TabPanels>
+                                <TabPanel>
+                                    <VStack p = {4}>
+                                        {/*
+                                        This card is an example for demonstration purposes only and
+                                        therefore MUST be overwritten to be able to work w/ backend 
+                                        using props & mapping especially for mutliple events/tags
+                                        */}
+                                        <Box 
+                                            w = "80%" 
+                                            h = "40"
+                                            display = "flex"
+                                        >
+                                            <Card 
+                                                w = "100%" 
+                                                h = "100%"
+                                                borderRadius="sm"
+                                            > 
+                                                <Flex 
+                                                    h="100%" 
+                                                    align="center" 
+                                                    px={4} 
+                                                    positions = "relative"
+                                                >
+                                                    <VStack 
+                                                        align="start" 
+                                                        spacing={1}
+                                                    >
+                                                        <HStack spacing={2}>
+                                                            <Icon as={IoCalendarSharp} />
+                                                            <Text fontSize="md"> May 4th, 2026 @ 10 P.M. </Text>
+                                                        </HStack>
+
+                                                        <HStack spacing={2}>
+                                                            <Icon as={FaLocationDot} />
+                                                            <Text fontSize="md">Example Location</Text>
+                                                        </HStack>
+
+                                                        <HStack spacing={2}>
+                                                            <Icon as={IoPersonSharp} />
+                                                            <Text fontSize="md"> 20/40 attendees </Text>
+                                                        </HStack>
+                                                    </VStack>
+
+                                                    <Box position="absolute" left="50%" transform="translateX(-50%)">
+                                                        <HStack>
+                                                            <Text fontSize="md" fontWeight="bold">
+                                                                Tags: 
+                                                            </Text>
+                                                            <Tag bg = "#D9D9D9">
+                                                                Workshop
+                                                            </Tag>
+                                                            <Tag bg = "#D9D9D9">
+                                                                ex1
+                                                            </Tag>
+                                                            <Tag bg = "#D9D9D9">
+                                                                ex2
+                                                            </Tag>
+                                                        </HStack>
+                                                    </Box>
+
+                                                    <Button bg = "#D9D9D9" px = "4%" borderRadius="lg" ml = "auto">
+                                                        View Event
+                                                    </Button>
+                                                </Flex>
+                                            </Card>
+                                        </Box>
+                                    </VStack>
+                                </TabPanel>
+                            </TabPanels>
+                        </Tabs>
                     </TabPanel>
                     <TabPanel>
                         {/* 
