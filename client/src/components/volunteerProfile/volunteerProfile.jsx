0cd425cb8ad91c8813c9e273ba9c5f325603999d
Ashres05 <ashres05@gmail.com>
2026-01-16 00:06:15 -0800

    Created Profile Management header section.

--- client/src/components/volunteerProfile/volunteerProfile.jsx
+++ client/src/components/volunteerProfile/volunteerProfile.jsx
@@ -1,11 +1,45 @@
-import { } from "react";
+import { React } from "react";
 
 import {
-  
+  Box,
+  Heading,
+  Flex,
+  Button,
+  Image,
+  Divider
 } from "@chakra-ui/react";
 
-export const volunteerProfile = () => {
+import buttonArrow from "../profilePage/buttonArrow.png";
+
+function profileManagement() {
   return (
-      <p>volunteerProfile</p>
+    <Box>
+      <Flex justify="space-between" align="center">
+        <Heading size="md">
+          Profile Management
+        </Heading>
+        <Flex gap="2">
+          <Button width="66px" height="28px" color="212121" backgroundColor="#D9D9D9" fontSize="16px" variant="outline" borderColor="#212121" borderWidth="1px" rightIcon={<Image src={buttonArrow} boxSize="12px"/>}>
+            <Flex gap="1" align="center">
+              Edit
+            </Flex>
+          </Button>
+          <Button width="55px" height="28px" color="212121" backgroundColor="#D9D9D9" fontSize="16px" variant="outline" borderColor="#212121" borderWidth="1px">
+            Save
+          </Button>
+        </Flex>
+      </Flex>
+
+      <Divider borderWidth="0.5px" borderColor="#000000" mt="4" mb="8"/>
+
+      
+
+    </Box>
+  )
+}
+
+export const VolunteerProfile = () => {
+  return (
+    profileManagement()
   );
 };
