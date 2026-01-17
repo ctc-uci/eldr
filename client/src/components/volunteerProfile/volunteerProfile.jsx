7d7363a1e654c8fba1ae1693dbe7e94071ae003b
Ashres05 <ashres05@gmail.com>
2026-01-16 18:09:20 -0800

    Added data display dashboards to the volunteer profile management page.

--- client/src/components/volunteerProfile/volunteerProfile.jsx
+++ client/src/components/volunteerProfile/volunteerProfile.jsx
@@ -15,7 +15,10 @@ import {
   IconButton,
   InputGroup,
   InputRightElement,
-  Link
+  Link,
+  Text,
+  VStack,
+  Stack
 } from "@chakra-ui/react";
 
 import buttonArrow from "../profilePage/buttonArrow.png";
@@ -23,8 +26,206 @@ import specializationsPlus from "../profilePage/specializationsPlus.png";
 import specializationsHighlights from "../profilePage/specializationsHighlights.png";
 import languagesHighlights from "../profilePage/languagesHighlights.png";
 import passwordEye from "../profilePage/passwordEye.png";
+import dashboardClock from "../profilePage/dashboardClock.png";
+import dashboardEventExpand from "../profilePage/dashboardEventExpand.png"
 
-function profileManagement() {
+const InfoManagement = () => {
+  return (
+    <SimpleGrid width="50%" columns={2} spacingX={20} spacingY={10}>
+        <FormControl>
+          <FormLabel fontWeight="bold">
+            First Name
+          </FormLabel>
+          <Input type="text" defaultValue="Peter" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+        </FormControl>
+        <FormControl>
+          <FormLabel fontWeight="bold">
+            Last Name
+          </FormLabel>
+          <Input type="text" defaultValue="Anteater" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+        </FormControl>
+        <FormControl>
+          <FormLabel fontWeight="bold">
+            Phone Number
+          </FormLabel>
+        <Input type="text" defaultValue="621-438-3991" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+      </FormControl>
+
+      <GridItem colSpan={2}>
+        <FormControl>
+          <FormLabel fontWeight="bold">
+            Specialization(s)
+          </FormLabel>
+          <Flex width="60%" minHeight="64px" p={2} borderWidth="2px" borderColor="black" borderRadius="md" align="center" justify="space-between">
+            <Image src={specializationsHighlights} height="29px"/>
+            <IconButton icon={<Image src={specializationsPlus} boxSize="24px"/>} variant="ghost" _hover={{bg: "transparent"}} isRound={true} _active={{transform: "scale(0.9)"}}/>
+          </Flex>
+        </FormControl>
+      </GridItem>
+
+      <GridItem colSpan={2}>
+        <FormControl>
+          <FormLabel fontWeight="bold">
+            Languages(s)
+          </FormLabel>
+          <Flex width="35%" minHeight="64px" p={2} borderWidth="2px" borderColor="black" borderRadius="md" align="center" justify="space-between">
+            <Image src={languagesHighlights} height="29px"/>
+          </Flex>
+        </FormControl>
+      </GridItem>
+
+      <FormControl>
+        <FormLabel fontWeight="bold">
+          Law School & Company
+        </FormLabel>
+        <Input type="text" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+      </FormControl>
+    </SimpleGrid>
+  )
+}
+
+const AccountManagement = () => {
+  return (
+    <SimpleGrid width="50%" columns={2} spacingX={20} spacingY={10} mb={20}>
+      <FormControl>
+        <FormLabel fontWeight="bold">
+          Email Address
+        </FormLabel>
+        <Input type="text" defaultValue="user@uci.edu" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+      </FormControl>
+      <FormControl>
+        <FormLabel fontWeight="bold">
+          Password
+        </FormLabel>
+
+        <Flex align="center" gap={3}>
+          <InputGroup flex="1">
+            <Input type="password" defaultValue="thisismypassword" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
+            <InputRightElement>
+              <IconButton icon={<Image src={passwordEye} boxSize="24px"/>} variant="ghost" _hover={{bg: "transparent"}} isRound={true} _active={{transform: "scale(0.9)"}}/>
+            </InputRightElement>
+          </InputGroup>
+          <Link color="#2E52D6" fontSize="14px" fontWeight="semibold" textDecoration="underline" whiteSpace="nowrap">
+            Reset Password
+          </Link>
+        </Flex>
+      </FormControl>
+    </SimpleGrid>
+  )
+}
+
+const DashboardManagement = () => {
+  return (
+    <SimpleGrid width="60%" columns={2} spacing={6} mb={10}>
+       
+       <Box p={6} borderRadius="2xl">
+          <Flex justify="space-between" align="center" mb={6} width="80%">
+            <Text fontWeight="bold" fontSize="18px">
+              Volunteer Hours
+            </Text>
+            <Button size="xs" bg="#D9D9D9" border="1px solid black" borderRadius={0} px={4}>
+                <Text fontSize="12px" fontWeight="bold">
+                  Clio
+                </Text>
+            </Button>
+          </Flex>
+
+          <Flex align="center">
+            <Image src={dashboardClock} boxSize="24px" mr={2}/>
+            <VStack spacing={0} mr={6} align="center" width="60px">
+               <Text fontSize="12px" color="#757575" fontWeight="bold">
+                Total
+              </Text>
+              <Text fontSize="12px" lineHeight="1">
+                25
+              </Text>
+            </VStack>
+
+            <Divider orientation="vertical" height="40px" borderColor="#000000" mr={8}/> {/* Divider for Data */}
+
+            <Flex width="100%" gap={6}>
+              <VStack align="start" spacing={0}>
+                  <Text fontSize="12px" color="#757575" fontWeight="bold" mb={1}>
+                      Case
+                  </Text>
+                  <Text fontSize="12px" lineHeight="1">
+                      15
+                  </Text>
+              </VStack>
+              <VStack align="start" spacing={0}>
+                  <Text fontSize="12px" color="#757575" fontWeight="bold" mb={1}>
+                      Clinic & Workshop
+                  </Text>
+                  <Text fontSize="12px" lineHeight="1">
+                      10
+                  </Text>
+              </VStack>
+            </Flex>
+          </Flex>
+       </Box>
+
+       <Box p={6} borderRadius="2xl" position="relative">
+          <Text fontWeight="bold" fontSize="18px" mb={6}>Event Count</Text>
+          <VStack spacing={4} align="stretch">
+            <Flex justify="space-between">
+                <Text fontSize="12px" color="#757575" fontWeight="bold">
+                  Case
+                </Text>
+                <Text fontSize="12px" color="#757575" fontWeight="bold">
+                  Clinic & Workshop
+                </Text>
+                <Text fontSize="12px">9</Text>
+            </Flex>
+            <Flex justify="flex-end" width="115%">
+              <IconButton icon={<Image src={dashboardEventExpand} boxSize="24px"/>} variant="ghost" _hover={{bg: "transparent"}} isRound={true} _active={{transform: "scale(0.9)"}}/>
+            </Flex>
+          </VStack>
+       </Box>
+
+       <Box p={6} borderRadius="2xl">
+          <Text fontWeight="bold" fontSize="18px" mb={2}>
+            Event Log
+          </Text>
+          <Flex gap={3} mb={5}>
+            <Button size="xs" bg="#D9D9D9" border="1px solid black" borderRadius="full" px={4}>
+                Recent
+            </Button>
+            <Button size="xs" bg="white" border="1px solid #757575" borderRadius="full" px={4} color="#757575">
+                Repeat
+            </Button>
+          </Flex>
+          <Stack spacing={3}>
+            <Box bg="white" p={3} borderRadius="lg" boxShadow="sm">
+                <Text fontSize="12px" fontWeight="medium">
+                  December 2, 2025 - [Clinic Name]
+                </Text>
+            </Box>
+            <Box bg="white" p={3} borderRadius="lg">
+                <Text fontSize="12px" fontWeight="medium">
+                  November 31, 2025 - [Workshop Name]
+                </Text>
+            </Box>
+          </Stack>
+       </Box>
+
+       <Box p={6} borderRadius="2xl">
+          <Text fontWeight="bold" fontSize="18px" mb={10}>
+            Active Date
+          </Text>
+          <Flex justify="space-between" align="center">
+            <Text fontSize="12px" fontWeight="bold" color="#757575">
+              Last Logged
+            </Text>
+            <Text fontSize="12px">
+              December 2, 2025
+            </Text>
+          </Flex>
+       </Box>
+    </SimpleGrid>
+  )
+}
+
+const profileManagement = () => {
   return (
     <Box>
       <Flex justify="space-between" align="center">
@@ -45,83 +246,13 @@ function profileManagement() {
 
       <Divider borderWidth="0.5px" borderColor="#000000" mt={8} mb={10}/>
 
-      <SimpleGrid width="50%" columns={2} spacingX={20} spacingY={10}>
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            First Name
-          </FormLabel>
-          <Input type="text" defaultValue="Peter" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-        </FormControl>
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            Last Name
-          </FormLabel>
-          <Input type="text" defaultValue="Anteater" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-        </FormControl>
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            Phone Number
-          </FormLabel>
-        <Input type="text" defaultValue="621-438-3991" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-      </FormControl>
+      <InfoManagement />
 
-        <GridItem colSpan={2}>
-          <FormControl>
-            <FormLabel fontWeight="bold">
-              Specialization(s)
-            </FormLabel>
-            <Flex width="60%" minHeight="64px" p={2} borderWidth="2px" borderColor="black" borderRadius="md" align="center" justify="space-between">
-              <Image src={specializationsHighlights} height="29px"/>
-              <IconButton icon={<Image src={specializationsPlus} boxSize="24px"/>} variant="ghost" _hover={{bg: "transparent"}} isRound={true} _active={{transform: "scale(0.9)"}}/>
-            </Flex>
-          </FormControl>
-        </GridItem>
+      <Box></Box> {/* empty Box to reset column span*/}
 
-        <GridItem colSpan={2}>
-          <FormControl>
-            <FormLabel fontWeight="bold">
-              Languages(s)
-            </FormLabel>
-            <Flex width="35%" minHeight="64px" p={2} borderWidth="2px" borderColor="black" borderRadius="md" align="center" justify="space-between">
-              <Image src={languagesHighlights} height="29px"/>
-            </Flex>
-          </FormControl>
-        </GridItem>
+      <DashboardManagement />
 
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            Law School & Company
-          </FormLabel>
-          <Input type="text" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-        </FormControl>
-
-        {/* empty Box to reset column span*/}
-        <Box></Box>
-
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            Email Address
-          </FormLabel>
-          <Input type="text" defaultValue="user@uci.edu" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-        </FormControl>
-        <FormControl>
-          <FormLabel fontWeight="bold">
-            Password
-          </FormLabel>
-
-          <Flex width="133%" align="center" gap={3}>
-            <InputGroup flex="1">
-              <Input type="password" defaultValue="thisismypassword" borderWidth="2px" borderColor="#000000" _hover="#000000"/>
-              <InputRightElement>
-                <IconButton icon={<Image src={passwordEye} boxSize="24px"/>} variant="ghost" _hover={{bg: "transparent"}} isRound={true} _active={{transform: "scale(0.9)"}}/>
-              </InputRightElement>
-            </InputGroup>
-            <Link color="#2E52D6" fontSize="14px" fontWeight="semibold" textDecoration="underline" whiteSpace="nowrap">
-              Reset Password
-            </Link>
-          </Flex>
-        </FormControl>
-      </SimpleGrid>
+      <AccountManagement />
     </Box>
   )
 }
