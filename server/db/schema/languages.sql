02bff74fc40fc1324fba5d632c60db02006ee6f1
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2026-01-22 03:47:36 -0800

    fix: removed proficiency from languages, added it to specific junction tables

--- server/db/schema/languages.sql
+++ server/db/schema/languages.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS public.languages
 (
     id serial,
     language TEXT NOT NULL UNIQUE,
-    proficiency PROFICIENCY_LEVEL NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -12,6 +11,7 @@ CREATE TABLE IF NOT EXISTS public.case_languages
 (
     case_id INT NOT NULL,
     language_id INT NOT NULL,
+    proficiency PROFICIENCY_LEVEL NOT NULL,
     PRIMARY KEY (case_id, language_id),
     FOREIGN KEY (case_id) REFERENCES public.cases(id) ON DELETE CASCADE,
     FOREIGN KEY (language_id) REFERENCES public.languages(id) ON DELETE CASCADE
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS public.clinic_languages
 (
     clinic_id INT NOT NULL,
     language_id INT NOT NULL,
+    proficiency PROFICIENCY_LEVEL NOT NULL,
     PRIMARY KEY (clinic_id, language_id),
     FOREIGN KEY (clinic_id) REFERENCES public.clinics(id) ON DELETE CASCADE,
     FOREIGN KEY (language_id) REFERENCES public.languages(id) ON DELETE CASCADE
@@ -30,6 +31,7 @@ CREATE TABLE IF NOT EXISTS public.workshop_languages
 (
     workshop_id INT NOT NULL,
     language_id INT NOT NULL,
+    proficiency PROFICIENCY_LEVEL NOT NULL,
     PRIMARY KEY (workshop_id, language_id),
     FOREIGN KEY (workshop_id) REFERENCES public.workshops(id) ON DELETE CASCADE,
     FOREIGN KEY (language_id) REFERENCES public.languages(id) ON DELETE CASCADE
@@ -39,6 +41,7 @@ CREATE TABLE IF NOT EXISTS public.volunteer_language
 (
     volunteer_id INT NOT NULL,
     language_id INT NOT NULL,
+    proficiency PROFICIENCY_LEVEL NOT NULL,
     PRIMARY KEY (volunteer_id, language_id),
     FOREIGN KEY (volunteer_id) REFERENCES public.volunteers(id) ON DELETE CASCADE,
     FOREIGN KEY (language_id) REFERENCES public.languages(id) ON DELETE CASCADE
