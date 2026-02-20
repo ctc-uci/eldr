cc3c624dc808bed0888e41ee3391bd146e3b87cd
Rafael <therafaelfirme@gmail.com>
2026-02-20 12:56:45 -0800

    fix: when succesfully created account you can now go back to login page

--- client/src/components/volunteerLogin/volunteerLogin.tsx
+++ client/src/components/volunteerLogin/volunteerLogin.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 import { Flex } from "@chakra-ui/react";
 
-import NotaryStep from "./steps/NotaryStep";
-import CreateAccountStep from "./steps/CreateAccountStep";
 import BackgroundStep from "./steps/BackgroundStep";
+import CreateAccountStep from "./steps/CreateAccountStep";
 import LanguageStep from "./steps/LanguageStep";
 import LawInterestStep from "./steps/LawInterestStep";
 import LoginStep from "./steps/LoginStep";
+import NotaryStep from "./steps/NotaryStep";
 import SuccessStep from "./steps/SuccessStep";
 import WelcomeStep from "./steps/WelcomeStep";
 
@@ -18,15 +18,33 @@ export const VolunteerLogin = () => {
   const back = () => setStepIndex((i) => i - 1);
 
   return (
-    <Flex w="100vw" h="100vh">
+    <Flex
+      w="100vw"
+      h="100vh"
+    >
       {stepIndex === 0 && <WelcomeStep onNext={next} />}
-      {stepIndex === 1 && <LoginStep onNext={next} onBack={back} />}
-      {stepIndex === 2 && <CreateAccountStep onNext={next} onBack={back} />}
+      {stepIndex === 1 && (
+        <LoginStep
+          onNext={next}
+          onBack={back}
+        />
+      )}
+      {stepIndex === 2 && (
+        <CreateAccountStep
+          onNext={next}
+          onBack={back}
+        />
+      )}
       {stepIndex === 3 && <LanguageStep onNext={next} />}
-      {stepIndex === 4 && <LawInterestStep onNext={next} onBack={back} />}
+      {stepIndex === 4 && (
+        <LawInterestStep
+          onNext={next}
+          onBack={back}
+        />
+      )}
       {stepIndex === 5 && <NotaryStep onNext={next} />}
       {stepIndex === 6 && <BackgroundStep onNext={next} />}
-      {stepIndex === 7 && <SuccessStep />}
+      {stepIndex === 7 && <SuccessStep onNext={() => setStepIndex(1)} />}
     </Flex>
   );
 };
