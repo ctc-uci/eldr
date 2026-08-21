c8aefaafc11b2551067b16151af1039fd90b395c
Joshua Sullivan <joshee.sullivan@gmail.com>
2026-08-21 01:04:08 -0700

    style: remove duplicate token assignment

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -65,8 +65,6 @@ export const verifyRole = (requiredRole: string | string[]) => {
 
       res.locals.decodedToken = decodedToken;
 
-      res.locals.decodedToken = decodedToken;
-
       const users = await db.query(
         "SELECT * FROM users WHERE firebase_uid = $1 LIMIT 1",
         [decodedToken.uid]
